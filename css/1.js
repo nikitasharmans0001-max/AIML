3119511cfac976f7261ee7991d9494b3382a2bc4
nikita <nikitasharma@gmail.com>
2026-02-21 12:34:07 +0530

    first

---
+++ css/1.js
@@ -0,0 +1,8 @@
+<html>
+<head></head>
+<script>
+<body>
+document.write("this is first web page");
+</body>
+</script>
+</html>
---
+++ css/lec1.js
@@ -0,0 +1,5 @@
+let age=23
+if(age>18)
+{
+    console.log("You are a valid voter")
+}
